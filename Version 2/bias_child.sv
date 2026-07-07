f836325d891f6832f5fa81000973ee5086e1980b
Pham Van Hai Phuoc <phamphuoc3105@gmail.com>
2026-07-07 14:21:20 +0700

    Add files via upload

---
+++ Version 2/bias_child.sv
@@ -0,0 +1,42 @@
+`timescale 1ns/1ps
+`default_nettype none
+
+module bias_child (
+    input logic clk,
+    input logic rst,
+
+    input logic signed [15:0] bias_scalar_in,
+    output logic bias_Z_valid_out, 
+    input wire signed [15:0] bias_sys_data_in, // data from systolic array
+    input wire bias_sys_valid_in, // valid signal from the systolic array
+
+    output logic signed [15:0] bias_z_data_out,
+    output logic bias_overflow_out
+);
+    logic signed [15:0] z_pre_activation;
+    logic add_overflow;
+
+    fxp_add add_inst(
+        .ina(bias_sys_data_in),
+        .inb(bias_scalar_in),
+        .out(z_pre_activation),
+        .overflow(add_overflow)
+    );
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            bias_Z_valid_out <= 1'b0;
+            bias_z_data_out  <= 16'b0;
+            bias_overflow_out <= 1'b0;
+        end else begin
+            if (bias_sys_valid_in) begin
+                bias_Z_valid_out  <= 1'b1;
+                bias_z_data_out   <= z_pre_activation;
+                bias_overflow_out <= bias_overflow_out | add_overflow; // sticky
+            end else begin
+                bias_Z_valid_out  <= 1'b0;
+                bias_z_data_out   <= 16'b0;
+            end
+        end
+    end
+
+endmodule
